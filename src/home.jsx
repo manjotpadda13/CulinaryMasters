5626911a404f06d5235bb975f39d71e4cde5df44
Ryan Persaud <ryanpersaud811@gmail.com>
2024-01-18 19:51:35 -0500

    added basic homepage

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,49 @@
-// import './App.css'
+import React from 'react';
+import './Home.css'; // Import the external CSS file
 
 function Home() {
   return (
     <>
-      <h1>Culinary Masters</h1>
-      </>
-  )
+      <div className='header-sec'>
+        <h1>Culinary Masters</h1>
+        <input type="text" placeholder="Search.." />
+        <button className='srch-btn'>search</button>
+        <div className="dropdown">
+          <button className="dropbtn">Dropdown</button>
+          <div className="dropdown-content">
+            <a href="#">Vegan</a>
+            <a href="#">Desserts</a>
+            <a href="#">Appetizers</a>
+          </div>
+        </div>
+        <div className='row-one'>
+          <h3>Healthy</h3>
+          <div className='card'>
+            <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
+            <div className="container">
+              <h4><b>Dish One</b></h4>
+              <p>Ingredients:</p>
+            </div>
+          </div>
+          {/* card 2 */}
+          <div className='row-one'>
+          <h3>Healthy</h3>
+          <div className='card'>
+            <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
+            <div className="container">
+              <h4><b>Dish One</b></h4>
+              <p>Ingredients:</p>
+            </div>
+          </div>
+        </div>
+        {/* card 3 */}
+        
+        </div>
+        
+        
+      </div>
+    </>
+  );
 }
 
 export default Home;
