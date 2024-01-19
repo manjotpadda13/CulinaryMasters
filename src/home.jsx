315e73dbfc85cab183c8a1a61067ee96aa0b4a2c
mk10100 <mohamedkhalil10100@gmail.com>
2024-01-18 20:53:13 -0500

    fixed home

--- src/home.jsx
+++ src/home.jsx
@@ -1,53 +1,53 @@
-import React from 'react';
-import './Home.css'; // Import the external CSS file
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import SignUpPage from './SignUpPage';
+// import React from 'react';
+// import './Home.css'; // Import the external CSS file
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import LoginPage from './LoginPage';
+// import SignUpPage from './SignUpPage';
 
-function Home() {
-  return (
-    <>
-      <div className='header-sec'>
-        <h1>Culinary Masters</h1>
-        <input type="text" placeholder="Search.." />
-        <button className='srch-btn'>search</button>
-        <div className="dropdown">
-          <button className="dropbtn">Dropdown</button>
-          <div className="dropdown-content">
-            <a href="#">Vegan</a>
-            <a href="#">Desserts</a>
-            <a href="#">Appetizers</a>
-          </div>
-        </div>
-        <div className='row-one'>
-          <h3>Healthy</h3>
-          <div className='card'>
-            <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
-            <div className="container">
-              <h4><b>Dish One</b></h4>
-              <p>Ingredients:</p>
-            </div>
-          </div>
-          {/* card 2 */}
-          <div className='row-one'>
-          <h3>Healthy</h3>
-          <div className='card'>
-            <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
-            <div className="container">
-              <h4><b>Dish One</b></h4>
-              <p>Ingredients:</p>
-            </div>
-          </div>
-        </div>
-        {/* card 3 */}
+// function Home() {
+//   return (
+//     <>
+//       <div className='header-sec'>
+//         <h1>Culinary Masters</h1>
+//         <input type="text" placeholder="Search.." />
+//         <button className='srch-btn'>search</button>
+//         <div className="dropdown">
+//           <button className="dropbtn">Dropdown</button>
+//           <div className="dropdown-content">
+//             <a href="#">Vegan</a>
+//             <a href="#">Desserts</a>
+//             <a href="#">Appetizers</a>
+//           </div>
+//         </div>
+//         <div className='row-one'>
+//           <h3>Healthy</h3>
+//           <div className='card'>
+//             <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
+//             <div className="container">
+//               <h4><b>Dish One</b></h4>
+//               <p>Ingredients:</p>
+//             </div>
+//           </div>
+//           {/* card 2 */}
+//           <div className='row-one'>
+//           <h3>Healthy</h3>
+//           <div className='card'>
+//             <img src="https://picsum.photos/200" alt="Dish" style={{ width: '100%' }} />
+//             <div className="container">
+//               <h4><b>Dish One</b></h4>
+//               <p>Ingredients:</p>
+//             </div>
+//           </div>
+//         </div>
+//         {/* card 3 */}
         
-        </div>
+//         </div>
         
         
-      </div>
-    </>
-  );
-}
+//       </div>
+//     </>
+//   );
+// }
 
 
 
