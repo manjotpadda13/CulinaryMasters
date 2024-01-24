0c1fe34a0d387a230e64847f0479b200ed912b9a
Ryan Persaud <ryanpersaud811@gmail.com>
2024-01-23 21:47:46 -0500

    added routes

---
+++ src/components/landingPage/landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './components/login/login.css';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Landing</h1>
+      
+
+      
+    </div>
+  );
+};
+
+export default LandingPage;
